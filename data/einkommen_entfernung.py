0ef3490304615de435f04f3a21257b6362311ec1
Michell Skorna <michell.skorna@gmail.com>
2019-06-16 18:53:18 +0200

    orschgeign sind des

--- data/einkommen_entfernung.py
+++ data/einkommen_entfernung.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*- 
 # https://github.com/brandonxiang/geojson-python-utils
 # https://gis.stackexchange.com/questions/73768/converting-geojson-to-python-objects
-'''
 import pygeoj
 import re
 from geopy.geocoders import Nominatim
@@ -20,7 +19,7 @@ hamburg_coords = pygeoj.load("hamburg_city_districts.geojson")
 
 with open('h_einkommen_distanz.csv', 'w', encoding='utf-8') as csv_file: # Datei anlegen
         writer = csv.writer(csv_file,lineterminator='\n')
-        writer.writerow(["Stadtgebiet","Stadtbezirk","Stadt","Land","Entfernung","Gesamtbetrag der Einkünfte je Steuerpflichtigen im Jahr"])
+        writer.writerow(["Stadtgebiet","Gesamtbetrag der Einkünfte je Steuerpflichtigen im Jahr","Entfernung"])
 
 for feature in hamburg_coords:
 
@@ -49,15 +48,35 @@ for feature in hamburg_coords:
 
     # DATEN VORBEREITEN FÜR CSV
     skorna_array = koord_str.split(', ')
-    skorna_array.append(entfernung)
     print(skorna_array) # Super krasses Array mit allen wichtigen Informationen, yo.
-    if len(skorna_array) > 5:
-            print("Removed: "+skorna_array[2])
-            del skorna_array[2]
+    while len(skorna_array) > 1:
+            del skorna_array[1]
             print(skorna_array)
+    print(skorna_array)
 
     # WRITE INCOME FROM OTHER CSV
-    hamburg_einkommen = genfromtxt('hamburg_einkommen.csv', delimiter=';')
+
+    # if skorna_array[0] = row_in hamburg_einkommen.csv
+        # add einkommen
+    with open('hamburg_einkommen.csv', 'r', encoding='utf-8') as ham_ek:
+        csvReader = csv.reader(ham_ek)
+        for row in csvReader:
+            important_row = str(row).split(';')
+            sgb = re.sub('[:\'}]', '', important_row[0]) # Stadtgebiet
+            sgb = sgb.replace('[','')
+            sgb = sgb.replace('Ã¼','ü')
+            sgb = sgb.replace('Ã¶','ö')
+            sgb = sgb.replace('Ã¤','ä')
+            sgb = sgb.replace('ÃŸ','ß')
+            sgb = sgb.replace('.','. ') # St.Pauli / St.Georg --> St. Pauli  / St. Georg
+            if (skorna_array[0] == sgb) or ((skorna_array[0] in sgb) and ("und" in sgb)): # Statistik-Nord: Fick dich für "Waltershof und Finkenwerder;31649,00"
+                super_ugly_coding = important_row[1].split(',') # was zum Fick: '94234, 00]'
+                einkommen = re.sub('[:\'}]', '', super_ugly_coding[0]) # Einkommen
+                skorna_array.append(einkommen)
+    print(skorna_array)
+
+    skorna_array.append(entfernung)
+    print(skorna_array)
 
     # CSV SCHREIBEN
     with open('h_einkommen_distanz.csv', 'a', encoding='utf-8') as csv_file: # append
@@ -66,4 +85,3 @@ for feature in hamburg_coords:
 
 # For-Loop-End
 print("\r\nFinished")
-'''
