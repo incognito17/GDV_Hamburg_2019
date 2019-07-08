89ae19ccc0e99e9b5f19c17a95e53196357b178c
incognito17 <1532130@stud.hs-mannheim.de>
2019-07-08 19:20:48 +0200

    Kommentare angepasst (scatterplot.js)

--- ui/js/scatterplot.js
+++ ui/js/scatterplot.js
@@ -1,29 +1,20 @@
-//Call csv File
+//csv-Datei aufrufen
 d3.csv("data.csv", function (error, data) {
 
     let styl3;
     let styl4;
 
-    //Check For Error
+    //Wenn Fehler, dann auf Konsole ausgeben.
     if (error) console.log("Error");
 
-    /*
-    Check For Data
-    data.forEach(function (d) {
-        console.log("X Is "+ d.x);
-        console.log("Y Is "+ d.y);
-    });
-    */
-
-    //Create Margin
+    //Margin erstellen
     var margin = { top: 40, right: 20, bottom: 60, left: 100 },
         width = 960 - margin.left - margin.right,
         height = 600 - margin.top - margin.bottom;
     /*
-   define scale then followed by axis
+   Skalierung und Achsen definieren.
    */
-    // define x and y scales
-    // define x and y scales
+    // x und y Skalierungen definieren
     xMax = d3.max(data, function (d) {
         return +d.Entfernung;
     });
@@ -36,7 +27,7 @@ d3.csv("data.csv", function (error, data) {
     var yScale = d3.scale.linear().
     domain([0, yMax]).
     range([height, 0]);
-    // define x axis and y axis
+    // x und y Achsen definieren
     var xAxis = d3.svg.axis().
     scale(xScale).
     orient("bottom");
@@ -45,7 +36,7 @@ d3.csv("data.csv", function (error, data) {
     orient("left");
 
     /*
-   create svg element then append height and width and g which act as a container
+    SVG-Element erstellen, Höhe, Breite und "g" (welches als Container dient) anhängen
    */
     var svg = d3.select("#scatterplot").
     append("svg").
@@ -56,7 +47,7 @@ d3.csv("data.csv", function (error, data) {
     append("g").
     attr("transform", "translate(" + margin.left + "," + margin.right + ")");
 
-    // Draw xAxis
+    // x-Achse "zeichnen"
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
@@ -69,7 +60,7 @@ d3.csv("data.csv", function (error, data) {
         .style("font-weight", "bold")
         .text("Entfernung zur Stadtmitte in KM");
 
-    //Draw yAxis
+    //y-Achse "zeichnen"
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis);
@@ -90,15 +81,17 @@ d3.csv("data.csv", function (error, data) {
         .style("opacity", 0);
 
     /* create bar or bind data*/
-    //bind data
+    //Datenbindung
     svg.selectAll("circle")
         .data(data)
         //enter data
         .enter().
+    //die einzelnen Punkten mit Koordinaten aus Data anhängen
     append("circle")
-    //update data
         .attr("class", "circle")
+        // Id wegen gleichzeitigen "hover"-Effekt hinzufügen
         .attr("id", function (d) {
+            //Sonderfall wegen Namen von "StPauli" und "StGeorg" als ID
             let stadtteil = d.Stadtgebiet;
             if (stadtteil == "St. Pauli") {
                 stadtteil = "StPauli";
@@ -109,6 +102,7 @@ d3.csv("data.csv", function (error, data) {
         }).attr("cx", function (d) { return xScale(d.Entfernung); })
         .attr("cy", function (d) { return yScale(d.Einkünfte); })
         .attr("r", 8)
+        //Färben bei "mouseonver" Effekt
         .on("mouseover", function(d){
             let stadtteil = d.Stadtgebiet;
             if (stadtteil == "St. Pauli") {
@@ -116,16 +110,21 @@ d3.csv("data.csv", function (error, data) {
             } else if (stadtteil == "St. Georg") {
                 stadtteil = "StGeorg"
             }
+            //sich die Farbe der einzelnen Punkte als eine "globale" (nur für diese Datei) Variable merken
+            //damit die Punkte nicht konstant grün bleiben.
             styl3 = document.getElementById(stadtteil).style.fill;
             styl4 = document.getElementById(stadtteil+"2").style.fill;
             document.getElementById(stadtteil).style.fill="lime";
             document.getElementById(stadtteil+"2").style.fill="lime";
+            //Tooltip aufrufen
             div.transition()
                 .duration(100)
                 .style("opacity", .9);
+            //Die Informationen dem Tooltip mitgeben
             div.html(d.Stadtgebiet+"<br/>Einkommen: "+d.Einkünfte+" €<br/>Entfernung: "+d.Entfernung+" km")
                 .style("left", (d3.event.pageX) + "px")
                 .style("top", (d3.event.pageY - 60) + "px");
+            // bei "mouseout" den einzelnen Punkt in die ursprüngliche Farbe färben.
         }).on("mouseout", function(d){
             let stadtteil = d.Stadtgebiet;
             if (stadtteil == "St. Pauli") {
